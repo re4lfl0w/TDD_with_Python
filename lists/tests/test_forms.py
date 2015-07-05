4cf2da2fc5e9f3c2270ada96691b9ea07485a286
re4lfl0w <re4lfl0w@gmail.com>
2015-07-06 08:54:11 +0900

    폼 자체 save 메소드 사용

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,11 +1,9 @@
 from unittest import TestCase
 from lists.forms import ItemForm
+from lists.models import List, Item
+
 
 class ItemFormTest(TestCase):
-    # def test_form_renders_item_text_input(self):
-    #     form = ItemForm()
-    #     self.fail(form.as_p())
-
     def test_form_item_input_has_placeholder_and_css_classes(self):
         form = ItemForm()
         self.assertIn('placeholder="작업 아이템 입력"', form.as_p())
@@ -17,3 +15,11 @@ class ItemFormTest(TestCase):
         self.assertEqual(form.errors['text'],
                          ["You can't have an empty list item"]
         )
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
