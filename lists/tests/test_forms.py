ab786d287b46b7da0749d058291974ea714a105c
re4lfl0w <re4lfl0w@gmail.com>
2015-07-06 11:53:30 +0900

    뷰 계층에서의 중복 아이템 검증

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
-from lists.forms import ItemForm
+from lists.forms import (
+    EMPTY_LIST_ERROR, DUPLICATE_ITEM_ERROR, ExistingListItemForm, ItemForm
+)
 from lists.models import List, Item
 
 
@@ -20,6 +22,28 @@ class ItemFormTest(TestCase):
         list_ = List.objects.create()
         form = ItemForm(data={'text': 'do me'})
         new_item = form.save(for_list=list_)
+        # test_form_save_handles_saving_to_a_list() 가 동작하는데
+        # 그 중간에 test_form_validation_for_duplicate_items()가 실행되서 그런듯..
         self.assertEqual(new_item, Item.objects.first())
         self.assertEqual(new_item.text, 'do me')
         self.assertEqual(new_item.list, list_)
+
+
+class ExistingListItemFormTest(TestCase):
+    def test_form_renders_item_text_input(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_)
+        self.assertIn('placeholder="작업 아이템 입력"', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
+
+    def test_form_validation_for_duplicate_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='중복 금지!')
+        form = ExistingListItemForm(for_list=list_, data={'text': '중복 금지!'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
