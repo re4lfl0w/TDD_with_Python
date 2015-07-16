c81f0e30643dd54e9b893ec7f0a90fb9d69e03ed
re4lfl0w <re4lfl0w@gmail.com>
2015-07-16 20:59:19 +0900

    로그 코드 다듬기

--- superlists/settings.py
+++ superlists/settings.py
@@ -131,6 +131,12 @@ LOGGING = {
         'django': {
             'handlers': ['console'],
         },
+        'accounts': {
+            'handlers': ['console'],
+        },
+        'lists': {
+            'handlers': ['console'],
+        },
     },
     'root': {'level': 'INFO'},
 }
