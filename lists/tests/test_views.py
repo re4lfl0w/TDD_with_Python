9a2e8112f659d7afb643b48d55e3075058f13553
re4lfl0w <re4lfl0w@gmail.com>
2015-07-18 15:17:25 +0900

    new_list 뷰가 소유자를 할당하려 했지만 실패

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,8 +1,11 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.utils.html import escape
 from django.contrib.auth import get_user_model
 from lists.forms import ItemForm, EMPTY_LIST_ERROR, DUPLICATE_ITEM_ERROR, ExistingListItemForm
 from lists.models import Item, List
+from lists.views import new_list
+
 User = get_user_model()
 
 
@@ -163,6 +166,14 @@ class NewListTest(TestCase):
         self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
+
+    def test_list_owner_is_saved_if_user_is_authenticated(self):
+        request = HttpRequest()
+        request.user = User.objects.create(email='a@b.com')
+        request.POST['text'] = 'new list item'
+        new_list(request)
+        list_ = List.objects.first()
+        self.assertEqual(list_.owner, request.user)
 
 
 class MyListsTest(TestCase):
